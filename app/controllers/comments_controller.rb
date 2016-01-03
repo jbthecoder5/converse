7c05ca202e03daff309e61345e9d1bb50b5370c7
Joseph P. Bourne <kiratsuchi@live.com>
2016-01-03 02:58:05 -0800

    author attribute was added to the comments able to save name of users making a comment on a post. comment controller is completely updated. All bugs have been fixed.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,6 +14,6 @@ class CommentsController < ApplicationController
 	private
 
 	def comment_params
-		params.require(:comment).permit(:body, :user)
+		params.require(:comment).permit(:author, :body)
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151207183204) do
+ActiveRecord::Schema.define(version: 20160103101444) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20151207183204) do
     t.integer  "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "author"
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
